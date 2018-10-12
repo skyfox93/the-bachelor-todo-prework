6d87627c2e5a0ead01ecacd2bd210ac3c5ebc744
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-12 02:28:10 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,61 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winnerstats=""
+  data[season].each{ |object|
+    if object["status"].downcase=="winner"
+      winnerstats=object["name"].split(" ").first
+    end
+    
+  }
+winnerstats
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  winnerstats=""
+  data.each do |season,people|
+    people.each{ |person|
+      if person["occupation"]==occupation
+        winnerstats=person["name"]
+      end
+    
+    }
+end
+winnerstats
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  winnerstats=0
+  data.each do |season,people|
+    people.each{ |person|
+      if person["hometown"]==hometown
+        winnerstats+=1
+      end
+      
+    }
+  end
+winnerstats
 end
 
 def get_occupation(data, hometown)
-  # code here
+  winnerstats=""
+  data.each do |season,people|
+    people.each{ |person|
+      if (person["hometown"]==hometown && winnerstats=="")
+        winnerstats=person["occupation"]
+      end
+    
+    }
+end
+winnerstats
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  winnerstats={total: 0, count: 0}
+  data[season].each{ |person|
+      winnerstats[:total]+=person["age"].to_f
+        winnerstats[:count]+=1
+        
+      
+    }
+(winnerstats[:total]/winnerstats[:count]).round
 end
+ 
